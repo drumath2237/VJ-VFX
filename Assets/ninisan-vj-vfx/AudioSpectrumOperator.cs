06060a6b3441b2389c5c67b6d3fc5e9a656e7d80
drumath <2237k.drumath@gmail.com>
2020-06-24 18:45:57 +0900

    :sparkles:: 簡単なスクリプトを書いた

--- Assets/ninisan-vj-vfx/AudioSpectrumOperator.cs
+++ Assets/ninisan-vj-vfx/AudioSpectrumOperator.cs
@@ -1,7 +1,20 @@
-﻿namespace DefaultNamespace
+﻿using System;
+using UnityEngine;
+
+namespace NinisanVjVfx
 {
     public class AudioSpectrumOperator : UnityEngine.MonoBehaviour
     {
-        
+        [SerializeField] private AudioSpectrum spectrum;
+
+        private void Start()
+        {
+            
+        }
+
+        private void Update()
+        {
+            Debug.Log(spectrum.Levels.Length);
+        }
     }
 }
