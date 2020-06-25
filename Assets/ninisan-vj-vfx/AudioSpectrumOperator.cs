6426705c43bc31e30bdf42a5419ab584bbdf1ddf
drumath <2237k.drumath@gmail.com>
2020-06-25 13:40:29 +0900

    :sparkles: bunny追加

--- Assets/ninisan-vj-vfx/AudioSpectrumOperator.cs
+++ Assets/ninisan-vj-vfx/AudioSpectrumOperator.cs
@@ -6,15 +6,22 @@ namespace NinisanVjVfx
     public class AudioSpectrumOperator : UnityEngine.MonoBehaviour
     {
         [SerializeField] private AudioSpectrum spectrum;
+        [SerializeField] private GameObject cube;
+        [SerializeField] private float scaleBias;
+        [SerializeField] private float rotationBias;
+
 
         private void Start()
         {
-            
         }
 
         private void Update()
         {
             Debug.Log(spectrum.Levels.Length);
+
+            cube.transform.localScale = spectrum.Levels[1] * scaleBias * Vector3.one;
+            cube.transform.rotation *= Quaternion.AngleAxis(spectrum.Levels[5] *spectrum.Levels[5] * rotationBias, new Vector3(1, 1, 0));
+            cube.transform.rotation *= Quaternion.AngleAxis(1, Vector3.up);
         }
     }
 }
